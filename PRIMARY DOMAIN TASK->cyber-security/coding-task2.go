3eddc5111b3c843c49fbc0395711b5b2779c4c85
0mnichan <75354317+0mnichan@users.noreply.github.com>
2024-10-03 00:58:35 +0530

    Add files via upload

---
+++ PRIMARY DOMAIN TASK->cyber-security/coding-task2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+func hasher() {
+	var pwd string
+
+	fmt.Println("pls enter your pwd")
+	fmt.Scanln(&pwd)
+
+	hash := sha256.New()
+	hash.Write([]byte(pwd))
+	hashed := hex.EncodeToString(hash.Sum(nil))
+
+	fmt.Println("SHA-256 hash of your pwd is:", hashed)
+}
+
+func main() {
+	hasher()
+}
